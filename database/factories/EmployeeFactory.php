da849a74e0ec4f5083bf72e01927b3c032846daf
Alex Garrett-Smith <alex@codecourse.com>
2024-01-30 09:19:57 +0000

    Episode 04

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $name = fake()->firstName(),
+            'slug' => str()->slug($name),
+            'profile_photo_url' => fake()->imageUrl()
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
